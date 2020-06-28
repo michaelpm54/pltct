1483ffaf214fc1e3292497bbca0b430d585369a3
MPM <39412876+michaelpm54@users.noreply.github.com>
2020-06-28 10:37:21 +0100

    Remove duplicate definitions

--- lexer.c
+++ lexer.c
@@ -14,54 +14,7 @@
 
 #define ERR_MAX_LEN 128
 
-typedef enum TokenType_s
-{
-	TOKEN_NONE,
-	TOKEN_UNKNOWN,
-	TOKEN_EOF,
-	TOKEN_NUMBER,
-	TOKEN_IDENTIFIER,
-	TOKEN_KEYWORD,
-	TOKEN_MULTIPLY,
-	TOKEN_DIVIDE,
-	TOKEN_ADD,
-	TOKEN_SUBTRACT,
-	TOKEN_LESS,
-	TOKEN_GREATER,
-	TOKEN_GE,
-	TOKEN_LE,
-	TOKEN_EQUAL,
-	TOKEN_ASSIGN,
-	TOKEN_STRING,
-	TOKEN_NEWLINE,
-	TOKEN_ARITHMETIC_OPERATORS_BEGIN = TOKEN_MULTIPLY,
-	TOKEN_ARITHMETIC_OPERATORS_END = TOKEN_EQUAL,
-	TOKEN_COMPARISON_OPERATORS_BEGIN = TOKEN_LESS,
-	TOKEN_COMPARISON_OPERATORS_END = TOKEN_ASSIGN,
-} TokenType;
-
-typedef enum StopReason_e
-{
-	STOP_NONE,
-	STOP_EOF,
-	STOP_UNKNOWN_TOKEN,
-	STOP_INVALID_NUMBER,
-	STOP_BAD_FILE,
-	STOP_PEEK_EOF,
-	STOP_INVALID_STRING,
-} StopReason;
-
-typedef struct Lexer_s
-{
-	int pos;
-	long unsigned int size;
-	char c;
-	char *buf;
-	StopReason stop;
-	char *errMsg;
-	int line;
-	int column;
-} Lexer;
+#include "lexer.h"
 
 typedef struct Token_s
 {
